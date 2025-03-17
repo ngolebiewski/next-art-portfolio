00147532d37d7b37c68b5e5ce585523870d1f626
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-03-17 16:30:23 -0400

    home page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-// import Image from "next/image";
+// src/app/page.tsx
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Art Portfolio</h1>
+    <main className="relative min-h-screen">
+      {/* Hero Image */}
+      <div className="absolute inset-0">
+        <Image
+          src="/images/chinatown/1.jpg"
+          alt="Chinatown Hero"
+          fill
+          style={{ objectFit: "cover" }}
+          priority // Optionally add priority for initial load
+        />
+        <div className="absolute inset-0 bg-black opacity-0"></div> {/* Overlay */}
+      </div>
+
+      {/* Text Content */}
+      {/* <div className="relative z-10 flex flex-col items-center justify-center min-h-screen text-white">
+        <h1 className="text-4xl font-bold mb-4">Art Portfolio</h1>
+        <p className="text-lg">Welcome to my art portfolio.</p>
+      </div> */}
     </main>
   );
 }
